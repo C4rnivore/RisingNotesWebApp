24eae9de41caec0910e68e44364ecce26d0cc00a
C4rnivore <kirill.z13@mail.ru>
2023-07-11 11:44:33 +0500

    APP complete

--- src/Pages/ArtistPersonalPage/ArtistPersonalPage.js
+++ src/Pages/ArtistPersonalPage/ArtistPersonalPage.js
@@ -1,4 +1,6 @@
 import Sidebar from "../../Components/Sidebar/Sidebar";
+import Song from "../../Components/Song";
+import ProfileImage from '../../Images/pfpimg.png'
 import {Link} from "react-router-dom";
 import logo from '../../Images/login/logo.svg'
 
@@ -13,17 +15,68 @@ function ArtistPersonalPage() {
                     </div>
                     <div className="about-text-area">
                         <span className="artist-info-label">Информация о себе</span>
-                        <textarea className="artist-input-field" placeholder="Введите информацию о себе"/>
+                        <textarea draggable={"false"} className="artist-input-field" placeholder="Введите информацию о себе"/>
                     </div>
                 </section>
                 <section className="stat app-sec">
-                    Ссылки на соцсети
-                    Вконтакте <input type="text"/>
-                    Яндексмузыка <input type="text"/>
-                    Сайт <input type="text"/>
-                    <img src={logo} alt=""/>
+                    <div className="info-area">
+                        <span className="info-area-title span-title">Ссылки на соцсети</span>
+                        <ul className="links">
+                            <li className="link-field">
+                                <span className="link-label">
+                                    ВКонтакте
+                                </span>
+                                <input type="text" className="link-input" placeholder="Ссылка"/>
+                            </li>
+                            <li className="link-field">
+                                <span className="link-label">
+                                    Яндекс Музыка
+                                </span>
+                                <input type="text" className="link-input" placeholder="Ссылка"/>
+                            </li>
+                            <li className="link-field">
+                                <span className="link-label">
+                                    Сайт
+                                </span>
+                                <input type="text" className="link-input" placeholder="Ссылка"/>
+                            </li>
+                        </ul>
+                        <div className="stat-area">
+                            <span className="subs-label span-title">
+                                Подписчики
+                            </span>
+                            <span className="subs-count stat-value">
+                                200
+                            </span>
+                        </div>
+                        <div className="stat-area">
+                            <span className="subs-label span-title">
+                                Прослушиваний за месяц
+                            </span>
+                            <span className="subs-count stat-value">
+                                600
+                            </span>
+                        </div>
+                    </div>
+                    <img src={ProfileImage} alt="" className="pf-img"/>
                 </section>
-                <section className="songs app-sec"></section>
+                <section className="songs app-sec">
+                    <div className="songs-upper-panel">
+                        <span className="songs-label span-title">
+                            Все треки
+                        </span>
+                        <button className="add-btn">
+                            <span className="inner-btn-text">
+                                Добавить новую песню
+                            </span>
+                        </button>
+                    </div>
+                    <div className="songs-list">
+                        <Song/>
+                        <Song/>
+                        <Song/>
+                    </div>
+                </section>
             </div>
             <Sidebar/>
         </div>
