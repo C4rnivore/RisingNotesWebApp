0a7df9791bff26a1eca24486177a0c8b7e799d7c
C4rnivore <kirill.z13@mail.ru>
2023-10-31 20:33:18 +0500

    Sidebur initial update. Header small updates

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import pfp_placeholder from '../../Images/image-placeholder/pfp_placeholder.jpg'
 import logotype from '../../Images/logo.svg'
+import {Link, NavLink} from "react-router-dom";
 
 function Header() {
     const [pfp,setPfp] = useState(pfp_placeholder)
@@ -10,7 +11,7 @@ function Header() {
         <header className='header'>
             <div className="header_logo">
                 <img src={logo} alt="" />
-                <span className='logo_txt'>RISING NOTES</span>
+                <span className='logo_txt'><Link to={'/'} className='logo_link'>RISING NOTES</Link></span>
             </div>
             <div className='header_pfp'>
                 <span className="pfp_dropdown">&#9207;</span>
