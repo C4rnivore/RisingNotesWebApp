7a2e542ee3670bac9eaf4c3814f1d6e3bf1ed62e
C4rnivore <kirill.z13@mail.ru>
2023-12-28 23:41:49 +0500

    search fix

--- src/Components/SearchResults/SearchContent/SeacrhContent.jsx
+++ src/Components/SearchResults/SearchContent/SeacrhContent.jsx
@@ -38,126 +38,125 @@ function SearchContent(props){
                 playlists={searchResult.playlists} 
                 navType={props.navType}/>)
     }
-}
 
-
-function SearchAll(props){
-    const searchResult = props.searchResult
-    return(
-        <div className='search-res-container'>
-            <SearchTracks tracks={searchResult.tracks} artists={searchResult.artists}/>
-            <SearchAuthors artists={searchResult.artists}/>
-            <SearchPlaylists playlists={searchResult.playlists}/>
-        </div>)
-}
-
-function SearchTracks(props){
-    const tracks = props.tracks
-    if(tracks.length == 0 && props.navType == undefined)
-        return(<></>)
-
-    if(tracks.length == 0 && props.navType == 'Tracks')
-        return(<div className="search-tracks-top pink-highlight">Не найдено треков по запросу</div>)
-    
-    return(
-        <div>
-            <div className="search-tracks-top">
-                <span>Треки</span>
-                <button className='search-show-more'>
-                {tracks.length>5?
-                        <>
-                            <span>Смотреть все</span>
-                            <img src={arrowRight} alt="" />
-                        </>
-                        : <></>
-                }
-                </button>
-            </div>
-            <div className="search-tracks-content">
-                {tracks.map(track =>(
-                    <Song key={track.id} id={track.id} name={track.name} duration={track.durationMs} artist={track.artistName}/>
-                ))}
-            </div>
-        </div>)
-}
-
-function SearchAuthors(props){
-    const frontend_url = 'http://localhost:3000/'
-    const artists = props.artists
-
-    function clearQuery(){
-        props.clearFunc()
+    function SearchAll(props){
+        const searchResult = props.searchResult
+        return(
+            <div className='search-res-container'>
+                <SearchTracks tracks={searchResult.tracks} artists={searchResult.artists}/>
+                <SearchAuthors artists={searchResult.artists}/>
+                <SearchPlaylists playlists={searchResult.playlists}/>
+            </div>)
     }
-
-    if(artists.length == 0 && props.navType == undefined)
-    return(<></>)
-
-    if(artists.length == 0 && props.navType == 'Authors')
-        return(<div className="search-authors-top pink-highlight" >Не найдено исполнителей по запросу</div>)
-
-    return(
+    function SearchTracks(props){
+        const tracks = props.tracks
+        if(tracks.length == 0 && props.navType == undefined)
+            return(<></>)
+    
+        if(tracks.length == 0 && props.navType == 'Tracks')
+            return(<div className="search-tracks-top pink-highlight">Не найдено треков по запросу</div>)
+        
+        return(
             <div>
-                    <div className="search-authors-top">
-                    <span>Исполнители</span>
+                <div className="search-tracks-top">
+                    <span>Треки</span>
                     <button className='search-show-more'>
-                        {artists.length>5?
-                        <>
-                            <span>Смотреть все</span>
-                            <img src={arrowRight} alt="" />
-                        </>
-                        : <></>
-                        }
+                    {tracks.length>5?
+                            <>
+                                <span>Смотреть все</span>
+                                <img src={arrowRight} alt="" />
+                            </>
+                            : <></>
+                    }
                     </button>
                 </div>
-                <div className="search-authors-content">
-                    {artists.map((artist, index) => (
-                        <div key={index} className="search-artist-card">
-                            <Link to={`${frontend_url}artist/${artist.id}`} onClick={clearQuery}>
-                                <img src={api + `api/author/${artist.id}/logo?width=200&height=200`} alt="" />
-                            </Link>
-                            <span className='search-artist-name'>{artist.name}</span>
-                        </div>
+                <div className="search-tracks-content">
+                    {tracks.map(track =>(
+                        <Song key={track.id} id={track.id} name={track.name} duration={track.durationMs} artist={track.artistName}/>
                     ))}
                 </div>
-            </div>
-)}
-
-function SearchPlaylists(props){
-    const playlists = props.playlists
-
-    if(playlists.length == 0 && props.navType == undefined)
-    return(<></>)
-
-    if(playlists.length == 0 && props.navType == 'Playlists'){
-        return(<div className="search-playlists-top pink-highlight">Не найдено плейлистов по запросу</div>)
+            </div>)
     }
-    return(
-        <div>
-            <div className="search-playlists-top">
-                <span>Плейлисты</span>
-                <button className='search-show-more'>
-                {playlists.length>5?
-                        <>
-                            <span>Смотреть все</span>
-                            <img src={arrowRight} alt="" />
-                        </>
-                        : <></>
-                }
-                </button>
-            </div>
-            <div className="search-playlists-content">
-            {playlists.map(playlist =>(
-                        <Playlist key={playlist.id} id={playlist.id} />
-                    ))}
-            </div>
-        </div>)
+    
+    function SearchAuthors(props){
+        const frontend_url = 'http://localhost:3000/'
+        const artists = props.artists
+    
+        if(artists.length == 0 && props.navType == undefined)
+        return(<></>)
+    
+        if(artists.length == 0 && props.navType == 'Authors')
+            return(<div className="search-authors-top pink-highlight" >Не найдено исполнителей по запросу</div>)
+    
+        return(
+                <div>
+                        <div className="search-authors-top">
+                        <span>Исполнители</span>
+                        <button className='search-show-more'>
+                            {artists.length>5?
+                            <>
+                                <span>Смотреть все</span>
+                                <img src={arrowRight} alt="" />
+                            </>
+                            : <></>
+                            }
+                        </button>
+                    </div>
+                    <div className="search-authors-content">
+                        {artists.map((artist, index) => (
+                            <div key={index} className="search-artist-card">
+                                <Link to={`${frontend_url}artist/${artist.id}`} onClick={clearQuery}>
+                                    <img src={api + `api/author/${artist.id}/logo?width=200&height=200`} alt="" />
+                                </Link>
+                                <span className='search-artist-name'>{artist.name}</span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+    )}
+    
+    function SearchPlaylists(props){
+        const playlists = props.playlists
+    
+        if(playlists.length == 0 && props.navType == undefined)
+        return(<></>)
+    
+        if(playlists.length == 0 && props.navType == 'Playlists'){
+            return(<div className="search-playlists-top pink-highlight">Не найдено плейлистов по запросу</div>)
+        }
+        return(
+            <div>
+                <div className="search-playlists-top">
+                    <span>Плейлисты</span>
+                    <button className='search-show-more'>
+                    {playlists.length>5?
+                            <>
+                                <span>Смотреть все</span>
+                                <img src={arrowRight} alt="" />
+                            </>
+                            : <></>
+                    }
+                    </button>
+                </div>
+                <div className="search-playlists-content">
+                {playlists.map(playlist =>(
+                            <Playlist key={playlist.id} id={playlist.id} />
+                        ))}
+                </div>
+            </div>)
+    }
+    
+    function NotFoundPage(props){
+        return(
+            <div>
+                Ничего не найдено
+            </div>)
+    }
 }
 
-function NotFoundPage(props){
-    return(
-        <div>
-            Ничего не найдено
-        </div>)
-}
+
+
+
+
 
 export default SearchContent
